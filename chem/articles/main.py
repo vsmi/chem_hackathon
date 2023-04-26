d987aa545608ae4bc6ef29374e83d2b277df630f
viktorialegchikova <vika.smirnowa@yandex.ru>
2023-04-26 07:39:11 +0300

    test script for searching articles by key word

--- chem/articles/main.py
+++ chem/articles/main.py
@@ -3,12 +3,10 @@ import json
 
 FILE = 'test.json'
 #FILE = 'key_words.json'
-# API_KEY = 'a2c9a2977a45bfe07ba0e0823bbcfc20'  # qarnavalvleg@gmail.com
-#API_KEY = '5d00a7f480732c7529ed954bc73a1f73'  # vikulek.smi@gmail.com
-
-
-# API_KEY = 'ab8689d71796916e3fea09000fc965b5'  # vleg.turk@gmail.com
-API_KEY = 'cce7489e8ac31351b35f70dfa735cd5b' # vika.smirnowa@yandex.ru
+# API_KEY = 'a2c9a2977a45bfe07ba0e0823bbcfc20'  # qar
+#API_KEY = '5d00a7f480732c7529ed954bc73a1f73'  # vikul
+# API_KEY = 'ab8689d71796916e3fea09000fc965b5'  # vleg
+API_KEY = 'cce7489e8ac31351b35f70dfa735cd5b' # vika
 # API_KEY = '8bdae1708a84c12dce6d2c5cd461739f' #Dima1
 
 
@@ -16,7 +14,7 @@ def find_articles(kw: str):
     search_query = scholarly.search_pubs(kw, year_low=2020, sort_by='date')
     lists_of_articles = []
     try:
-        for i in range(2):
+        for i in range(3):
             lists_of_articles.append(next(search_query))
     except StopIteration:
         print("STOP ITERATION")
@@ -48,49 +46,3 @@ with open(FILE, 'r') as f_for_read:
 
 
 
-
-
-
-
-#search_query = scholarly.search_pubs('hepatogenous poisoning', year_low=2020, sort_by='date')
-# try:
-#     search_query = scholarly.search_pubs('Perception of physical stability and center of mass of 3D objects')
-#     scholarly.pprint(next(search_query))
-# except StopIteration:
-
-# search_query = scholarly.search_pubs('hepatogenous poisoning', year_low=2000, sort_by='date')
-# with open('generator.txt', 'r') as f1:
-#     lst = []
-#     for line in f1:
-#         s = f1.readline()
-#         print(s)
-#         json_acceptable_string = s.replace("'", "\"")
-#         print(type(json_acceptable_string))
-#         d = json.loads(json_acceptable_string)
-#         print(type(d))
-#         # lst.append(d)
-#
-# search_query = iter(lst)
-# total = search_query.total_size
-# total = len(search_query)
-
-#iter1 = iter(range(5))
-
-# try:
-#     lst1 = []
-#     for i in range(3):
-#         print(f'{i=}')
-#         a = next(iter1)
-#         print(a)
-#         lst1.append(a)
-#     print('inside try', lst1)
-# except StopIteration:
-#     print("We are here")
-#     print('inside except', lst1)
-#
-# print('the end')
-# s = "{'muffin' : 'lolz', 'foo' : 'kitty'}"
-# json_acceptable_string = s.replace("'", "\"")
-# d = json.loads(json_acceptable_string)
-#
-# print(d, type(d))
